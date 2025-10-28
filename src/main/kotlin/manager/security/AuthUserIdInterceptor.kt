ee59a2bd5bd7e308a877a9be8e8d31e9c72e9ed6
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-10-28 20:16:04 -0300

    fix: change the client from web to rest jaja

--- src/main/kotlin/manager/security/AuthUserIdInterceptor.kt
+++ src/main/kotlin/manager/security/AuthUserIdInterceptor.kt
@@ -1,11 +1,15 @@
 package manager.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import manager.security.auth.response.GetTokenResponse
+import org.apache.http.HttpHost
+import org.apache.http.util.EntityUtils
+import org.elasticsearch.client.Request
+import org.elasticsearch.client.RestClient
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
@@ -30,19 +34,30 @@ class AuthUserIdInterceptor : HandlerInterceptor {
     }
 
     private fun getUserInfo(token: String): String {
-        val fullAuthUrl = "http://authorization_service:8080/authentication/validate-user"
+        val restClient =
+            RestClient
+                .builder(
+                    HttpHost("authorization_service", 8080),
+                ).build()
 
-        val response: GetTokenResponse =
-            WebClient
-                .builder()
-                .build()
-                .get()
-                .uri(fullAuthUrl)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
-                .retrieve()
-                .bodyToMono(GetTokenResponse::class.java)
-                .block()
-                ?: throw IllegalStateException("No se pudo obtener información del usuario desde Auth0")
-        return response.subject
+        try {
+            val request = Request("GET", "/authentication/validate-user")
+            request.addParameter(HttpHeaders.AUTHORIZATION, "Bearer $token")
+
+            val response = restClient.performRequest(request)
+            val responseBody =
+                EntityUtils
+                    .toString(response.entity)
+
+            val mapper =
+                ObjectMapper()
+            val getTokenResponse =
+                mapper.readValue(responseBody, GetTokenResponse::class.java)
+                    ?: throw IllegalStateException("No se pudo obtener información del usuario desde Auth0")
+
+            return getTokenResponse.subject
+        } finally {
+            restClient.close()
+        }
     }
 }
