9dacef90b5f89092f2d6c0fc97751e0a452ee57a
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-10-28 12:37:45 -0300

    format

--- src/main/kotlin/manager/security/WebConfig.kt
+++ src/main/kotlin/manager/security/WebConfig.kt
@@ -8,16 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig(
     private val currentUserIdResolver: CurrentUserIdResolver,
-    private val authUserIdInterceptor: AuthUserIdInterceptor
+    private val authUserIdInterceptor: AuthUserIdInterceptor,
 ) : WebMvcConfigurer {
-
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(currentUserIdResolver)
     }
 
     override fun addInterceptors(registry: org.springframework.web.servlet.config.annotation.InterceptorRegistry) {
-        registry.addInterceptor(authUserIdInterceptor)
+        registry
+            .addInterceptor(authUserIdInterceptor)
             .addPathPatterns("/**") // or specify paths
     }
 }
-
