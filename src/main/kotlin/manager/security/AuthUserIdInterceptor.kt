0b72455b64ea52af9b5263da389e2abf638cebd1
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-10-29 11:03:03 -0300

    fix: refactor the api call to a service

--- src/main/kotlin/manager/security/AuthUserIdInterceptor.kt
+++ src/main/kotlin/manager/security/AuthUserIdInterceptor.kt
@@ -2,14 +2,13 @@ package manager.security
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import manager.security.auth.response.GetTokenResponse
-import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestClient
 import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class AuthUserIdInterceptor : HandlerInterceptor {
+class AuthUserIdInterceptor(
+    private val validateTokenService: ValidateTokenService,
+) : HandlerInterceptor {
     override fun preHandle(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -18,67 +17,10 @@ class AuthUserIdInterceptor : HandlerInterceptor {
         val authHeader = request.getHeader("Authorization") ?: return true
         val token = authHeader.removePrefix("Bearer ").trim()
 
-        val userId = getUserInfo2(token)
+        val userId = validateTokenService.getUserInfo(token)
 
         request.setAttribute("userId", userId)
 
         return true
-    }
-
-//    private fun getRestClient(): ESRestClient =
-//        ESRestClient
-//            .builder(
-//                HttpHost("asset_service", 8080),
-//            ).build()
-
-//    private fun getUserInfo1(token: String): String {
-//        val restClient = getRestClient()
-//        try {
-//            val request = Request("GET", "/authentication/validate-user")
-//
-//            val options =
-//                RequestOptions.DEFAULT
-//                    .toBuilder()
-//                    .addHeader(HttpHeaders.AUTHORIZATION, "Bearer $token")
-//                    .build()
-//            request.options = options
-//
-//            println(request)
-//
-//            val response = restClient.performRequest(request)
-//            val status = response.statusLine.statusCode
-//
-//            if (status == 200) {
-//                val body = EntityUtils.toString(response.entity)
-//                val tokenResponse = ObjectMapper().readValue(body, GetTokenResponse::class.java)
-//                return tokenResponse.subject
-//            } else {
-//                val body = response.entity?.let { EntityUtils.toString(it) } ?: ""
-//                throw RuntimeException("Unexpected response: $status $body")
-//            }
-//        } finally {
-//            restClient.close()
-//        }
-//    }
-
-    fun getUserInfo2(token: String): String {
-        try {
-            val authorizationClient =
-                RestClient
-                    .builder()
-                    .baseUrl("http://authorization_service:8080/authentication/validate-user")
-                    .build()
-
-            val response: GetTokenResponse =
-                authorizationClient
-                    .get()
-                    .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
-                    .retrieve()
-                    .body(GetTokenResponse::class.java)!!
-
-            return response.subject
-        } catch (e: Exception) {
-            throw RuntimeException("Error validating user: ${e.message}")
-        }
     }
 }
---
+++ src/main/kotlin/manager/security/ValidateTokenService.kt
@@ -0,0 +1,31 @@
+package manager.security
+
+import manager.security.auth.response.GetTokenResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+
+@Service
+class ValidateTokenService {
+
+    fun getUserInfo(token: String): String {
+        try {
+            val authorizationClient =
+                RestClient
+                    .builder()
+                    .baseUrl("http://authorization_service:8080/authentication/validate-user")
+                    .build()
+
+            val response: GetTokenResponse =
+                authorizationClient
+                    .get()
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+                    .retrieve()
+                    .body(GetTokenResponse::class.java)!!
+
+            return response.subject
+        } catch (e: Exception) {
+            throw RuntimeException("Error validating user: ${e.message}")
+        }
+    }
+}
