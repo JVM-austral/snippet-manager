92e88badf2bd00bdc1fb3de276e3cf37e7b874b0
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-10-29 11:45:02 -0300

    fix: add a request factory

--- src/main/kotlin/manager/security/ValidateTokenService.kt
+++ src/main/kotlin/manager/security/ValidateTokenService.kt
@@ -2,6 +2,7 @@ package manager.security
 
 import manager.security.auth.response.GetTokenResponse
 import org.springframework.http.HttpHeaders
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 
@@ -10,6 +11,7 @@ class ValidateTokenService {
     private val authorizationClient: RestClient by lazy {
         RestClient
             .builder()
+            .requestFactory(HttpComponentsClientHttpRequestFactory())
             .baseUrl("http://authorization_service:8080")
             .build()
     }
